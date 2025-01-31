7a8779d6a7142b71ac39981e257aea499b95b518
ktyudha <ktyudha@gmail.com>
2025-01-31 10:05:05 +0700

    feat(user): slicing appoinment

--- src/modules/landing/history/index.tsx
+++ src/modules/landing/history/index.tsx
@@ -7,15 +7,11 @@ const LandingHistory: FunctionComponent = () => {
   const { medicals } = useGetAllMedical();
   return (
     <>
-      <section className="md:mx-auto bg-white min-h-screen max-w-screen-xl">
+      <section className="md:mx-auto bg-white max-w-screen-xl">
         <div className="mx-4 md:px-4 pt-4">
           {isEmpty(medicals) ? (
             <div className="flex flex-col items-center justify-center gap-3 h-full">
-              <img
-                src={EmptyIcon}
-                alt="Icon"
-                className="md:w-36 w-20 my-auto"
-              />
+              <img src={EmptyIcon} alt="Icon" className="md:w-36 w-20" />
               <h1 className="font-semibold md:text-lg text-base">
                 Tidak Ada Data
               </h1>
