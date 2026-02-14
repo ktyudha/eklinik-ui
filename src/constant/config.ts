50ee8a46f3a2557f1d0ea35d8d095aeb87b614d2
ktyudha <ktyudha@gmail.com>
2026-02-14 19:13:26 +0700

    chore: change base url

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "https://api-eklinik.ktyudha.site/api/v1";
+export const BASE_API_URL = "https://api-eklinik.inb.my.id/api/v1";
 // export const BASE_API_URL = "http://127.0.0.1:8000/api/v1";
 
 const config = {
